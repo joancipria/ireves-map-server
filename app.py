e0e06f3cee33bbc407f59a795672b526cbee3509
Joan Ciprià <joancipria@protonmail.com>
2022-06-17 11:08:05 +0200

    Add some comments and split code

--- app.py
+++ app.py
@@ -1,25 +1,30 @@
-# save this as app.py
+# Flask imports
 from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
-import json
-staticModelV1file = open('./models/static/v1.json')
-staticModelV1 = json.load(staticModelV1file)
 import requests
-
-from pop import *
-
 from markupsafe import escape
 
+# Server imports
+from population import *
+from static_models import *
+
+# Create server
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+#-----------------
+# --- API REST ---
+#-----------------
+
+"""
+/health/openroute
+Checks openroute service health
+"""
 @app.route('/health/openroute', methods=['POST', 'GET'])
 @cross_origin()
 def health():
-    """Comprueba status openroute"""
-
     headers = {
         'Accept': 'application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8',
         'Content-Type': 'application/json; charset=utf-8'
@@ -27,22 +32,17 @@ def health():
     call = requests.get('http://localhost:8080/ors/v2/health/', headers=headers)
     return call.json()
 
+"""
+/isochrones/:lat,:lng,:time
+Returns an isochron in GeoJSON format from the given latitude (:lat), longitude (:lng) and time (:time).
+"""
 @app.route('/isochrones', methods=['POST', 'GET'])
 @cross_origin()
 def isochrones():
     lat = request.args.get('lat')
     lng = request.args.get('lng')
     time = int(request.args.get('time'))
-    """Cálcula la isócrona en la posición indicada y de la extensión de tiempo proporcionada
 
-    Parameters:
-    lng (float): Longitud de la posición
-    lat (float): Latitud de la posición
-    tiempoEnMinutos (int): Extensión de la isocrona en minutos
-
-    Returns:
-    string:Isocrona, en forma de GeoJson y formato string
-    """
     body = {"locations":[[lng, lat]],"range":[time*60]}
 
     headers = {
@@ -52,6 +52,10 @@ def isochrones():
     call = requests.post('http://localhost:8080/ors/v2/isochrones/driving-car', json=body, headers=headers)
     return call.json()
 
+"""
+/population/:polygon
+Returns population of an area determined by a GeoJSON polygon (:polygon).
+"""
 @app.route('/population', methods=['POST', 'GET'])
 @cross_origin()
 def population():
@@ -59,6 +63,10 @@ def population():
     population = calcPop(polygon)
     return population
 
+"""
+/model/static/:version
+Returns the specified (:version) static model in JSON formart
+"""
 @app.route('/model/static/', methods=['POST', 'GET'])
 @cross_origin()
 def staticModel():
